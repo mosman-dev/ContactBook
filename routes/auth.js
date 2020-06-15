5ff793148128001031116869dc2d01d93aa003bc
Mahmoud Osman <mahmoudosman@Mahmouds-MacBook.local>
2020-06-14 18:42:08 -0700

    express server setup with initial routes

---
+++ routes/auth.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+// @route       GET /api/auth
+// @desc        Get logged in user
+// @access    Private
+
+router.get('/', (req, res) => {
+	res.send('Get logged in user');
+});
+
+// @route       POST /api/auth
+// @desc        Autheticate user + get token
+// @access    Public
+
+router.post('/', (req, res) => {
+	res.send('Autheticate user + get token');
+});
+
+module.exports = router;
---
+++ routes/contacts.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+// @route       GET /api/contacts
+// @desc        Get user contacts
+// @access    Private
+
+router.get('/', (req, res) => {
+	res.send('Get user contacts');
+});
+
+// @route       POST /api/auth
+// @desc        Add new contact
+// @access    Private
+
+router.post('/', (req, res) => {
+	res.send('Add new contact');
+});
+
+// @route       PUT /api/auth/:id
+// @desc        Update contact
+// @access    Private
+
+router.put('/:id', (req, res) => {
+	res.send('Update contact');
+});
+
+// @route       DELETE /api/auth/:id
+// @desc        Delete contact
+// @access    Private
+
+router.delete('/:id', (req, res) => {
+	res.send('Delete contact');
+});
+
+module.exports = router;

5169415f9844d0f84e7f3e0a852a4d9cfa52b2ab
Mahmoud Osman <mahmoudosman@Mahmouds-MacBook.local>
2020-06-20 20:30:53 -0700

    Preparing for deployment

--- server.js
+++ server.js
@@ -18,11 +18,12 @@ app.use('/api/contacts', require('./routes/contacts'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
 	// Set static folder
-	app.use(express.static('/client/build'));
 
-	app.get('*', (req, res) =>
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-	);
+	const root = require('path').join(__dirname, 'client', 'build');
+	app.use(express.static(root));
+	app.get('*', (req, res) => {
+		res.sendFile('index.html', { root });
+	});
 }
 
 const PORT = process.env.PORT || 5000;
